2c4c5f1c9fda6e84269dab8c143b020e7f4bb96f
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-05-30 22:10:12 +0300

    problem solved

---
+++ Task_015/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Please, enter ordinal number of the day of the week");
+int day = int.Parse(Console.ReadLine());
+if (day <= 0 || day >= 8)// || or && для двойного условия
+{
+    Console.WriteLine("Please, try enter 0 < number <= 7 again");
+    return;
+}
+else
+{
+    if(day >= 6)
+    {
+        Console.WriteLine("This is weekend, do something useful for yourself");
+    }
+    else
+    {
+        Console.WriteLine("Солнце еще высоко");
+    }
+}
